bfb43fb79e886257473b36bfe821be1b9615d58c
Sona1526 <sonabaskarans@gmail.com>
2025-05-05 17:07:35 +0530

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,13 +11,11 @@ import { BookingPageComponent } from './booking-page/booking-page.component';
 import { BookingConfirmationComponent } from './booking-confirmation/booking-confirmation.component';
 
 export const routes: Routes = [
- // {path:'' , redirectTo:'home', pathMatch:'full'},
-  //{path:'**' , redirectTo:'home', pathMatch:'full'},
   {path:'' , component:HomeComponent},
   {path:'myprojects' , component:MyprojectsComponent},
   {path:'hotelbooking' , component:HotelbookingComponent},
   {path:'hotels' , component:HotelsComponent},
-  {path:'hotelDetails/:id' , component:HoteldetailsComponent, data: { renderMode: 'no-pre-render' }},
+  {path:'hotelDetails/:id' , component:HoteldetailsComponent},
   {path:'booking' , component:BookingPageComponent},
   {path:'booking-confirmation' , component:BookingConfirmationComponent}
 ];
